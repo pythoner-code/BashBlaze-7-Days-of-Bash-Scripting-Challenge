f9ecebfb4fcb9609d842311c57fc1550b5bc2133
pythoner-code <himanshu22503@gmail.com>
2023-08-01 09:57:31 +0530

    day1 solution

---
+++ Challenges/day1_basics.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+#Task 1
+#This is the day 1 of BashBlaze-7-das of bash scripting challenge.
+
+#Task 2
+echo -e "Hello this is the solution script of day 1 task."
+
+#Task 3
+echo " "
+variable=12
+variales=14
+
+#Task 4
+sum=$(($variable + $variables))
+echo -e "sum of $variable and $variales is " 
+echo "$sum"
+
+#Task 5
+echo -e "My current shell is - $SHELL"
+echo -e "My current working directory is - $PWD"
+echo -e "My home directory is - $HOME"
+echo -e "My hostname is - $HOSTNAME"
+
+#Task 6
+echo -e "Files with all the .txt extensions are -"
+ls *.txt
